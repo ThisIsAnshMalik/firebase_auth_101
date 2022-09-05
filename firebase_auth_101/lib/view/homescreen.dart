4415cd2a9f6e21bd4b3e4f6644c1d52e26ef4832
Ansh Malik <anshmalik279@gmail.com>
2022-09-05 11:56:06 +0530

    removed back button from home  to login screen

--- firebase_auth_101/lib/view/homescreen.dart
+++ firebase_auth_101/lib/view/homescreen.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text("Home Screen"),
         actions: [
           IconButton(
